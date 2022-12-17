b290621aa43b4c6d3117449b25c20c420fc87a75
Daniel-PeGa <danielperezgarnic@gmail.com>
2022-12-17 14:38:18 -0600

    sum progress been done

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,29 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        phone: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+        users: [User]
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, phone: String!): User
+        login(email: String!, password: String!): Auth
+    }
+`;
+
+module.exports = typeDefs;
